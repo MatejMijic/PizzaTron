4037f16e52bb2e3d6edf9f9d24d67969cab2a2db
Matej Mijić <matej@prototyp.digital>
2023-11-25 21:58:44 +0100

    Project settings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./app'],
+        alias: {
+          '~/assets': './app/assets',
+          '~/modules': './app/modules',
+          '~/navigators': './app/navigators',
+        },
+      },
+    ],
+  ],
 };
